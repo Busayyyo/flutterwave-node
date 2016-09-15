63378a6bf5246620c7f869c20dd2a6477f8ecf2e
Ikedieze Onyeforo <iykedapotential@gmail.com>
2016-09-15 16:55:16 +0100

    addedd the library functions, tests and examples for account linking, validation and send payments

--- test/flutterwave.account.tests.js
+++ test/flutterwave.account.tests.js
@@ -19,7 +19,7 @@ describe('#FlutterwaveAccountTest', function () {
 		this.timeout(50000);
 	    var flutterwavebase = new flwBase('tk_NabYp2XjZ6G9WwdFruzK','tk_tdyrSMQo8a');
 		var flutterwaveaccount  = new flwAccount(flutterwavebase);
-		/*console.log*/(flutterwaveaccount.initiateRecurrentPayment('0690000001', function (err, res, body) { console.log(err,body);
+		/*console.log*/(flutterwaveaccount.initiateRecurrentPayment('0690000001', function (err, res, body) { //console.log(err,body);
 			expect(body).to.have.property('data'); 
 			expect(body.data.responseMessage).to.equal('Successful, pending OTP validation');
 			done(); 
@@ -28,19 +28,85 @@ describe('#FlutterwaveAccountTest', function () {
 	}); 
 
 
-	it('Should return "Successful, pending OTP validation"', function (done) { 
+	it('Should return "Approved Or Completed Successfully"', function (done) {
 		this.timeout(50000);
 	    var flutterwavebase = new flwBase('tk_NabYp2XjZ6G9WwdFruzK','tk_tdyrSMQo8a');
 		var flutterwaveaccount  = new flwAccount(flutterwavebase);
 		/*console.log*/(flutterwaveaccount.resolveAccount({
-			"destbankcode":"076",
-			"recipientaccount":"1040917513",
-		}, function (err, res, body) { console.log(err,body);
+			"destbankcode":"058",
+			"recipientaccount":"0921318712",
+		}, function (err, res, body) { //console.log(err,body);
 			expect(body).to.have.property('data'); 
-			expect(body.data.responseMessage).to.equal('Successful, pending OTP validation');
+			expect(body.data.responsemessage).to.equal('Approved Or Completed Successfully');
 			done(); 
 		}));
 	     
+	});
+
+	it('Should return "Sorry, that account has been linked. Please check and try again!"', function (done) {
+		this.timeout(50000);
+		var flutterwavebase = new flwBase('tk_NabYp2XjZ6G9WwdFruzK','tk_tdyrSMQo8a');
+		var flutterwaveaccount  = new flwAccount(flutterwavebase);
+		(flutterwaveaccount.linkAccount({
+			"accountnumber":"0690000001"
+		}, function (err, res, body) { console.log(err,body);
+			expect(body).to.have.property('data');
+			expect(body.data.responsemessage).to.equal('Sorry, that account has been linked. Please check and try again!');
+			done();
+		}));
+
+	});
+
+	it('Should return "responsecode R3"', function (done) {
+		this.timeout(50000);
+		var flutterwavebase = new flwBase('tk_NabYp2XjZ6G9WwdFruzK','tk_tdyrSMQo8a');
+		var flutterwaveaccount  = new flwAccount(flutterwavebase);
+		(flutterwaveaccount.validateAccountLinking({
+			"otp":"12345",
+			"otptype":"PHONE_OTP",
+			"relatedreference":"0690000001"
+		}, function (err, res, body) { console.log(err,body);
+			expect(body).to.have.property('data');
+			expect(body.data.responsecode).to.equal('R3');
+			done();
+		}));
+
+	});
+
+	it('Should return responsecode "00"', function (done) {
+		this.timeout(50000);
+		var flutterwavebase = new flwBase('tk_NabYp2XjZ6G9WwdFruzK','tk_tdyrSMQo8a');
+		var flutterwaveaccount  = new flwAccount(flutterwavebase);
+		(flutterwaveaccount.getLinkedAccounts({}, function (err, res, body) { // console.log(err,body);
+			expect(body).to.have.property('data');
+			expect(body.data).to.have.property('linkedaccounts');
+			expect(body.data.responsecode).to.equal('00');
+			done();
+		}));
+
+	});
+
+	it('Should return "responsecode R3"', function (done) {
+		this.timeout(50000);
+		var flutterwavebase = new flwBase('tk_NabYp2XjZ6G9WwdFruzK','tk_tdyrSMQo8a');
+		var flutterwaveaccount  = new flwAccount(flutterwavebase);
+		(flutterwaveaccount.sendPayment({
+			"accounttoken": "qRyPWB60dR63t8XDc97aEg",
+			"destbankcode": "044",
+			"uniquereference": "1234568901",
+			"country": "NIGERIA",
+			"currency": "NGN",
+			"transferamount": "100.00",
+			"narration": "Test Send Money",
+			"recipientname": "John Doe",
+			"sendername": "John Walker",
+			"recipientaccount": "0690000021"
+		}, function (err, res, body) { console.log(err,body);
+			expect(body).to.have.property('data');
+			expect(body.data.responsecode).to.equal('R3');
+			done();
+		}));
+
 	});
 
 
