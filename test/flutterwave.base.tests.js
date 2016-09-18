fa99a6542151e8b0b1cecbc875888acfb8f4de32
perfectmak <damiperfect@gmail.com>
2016-09-18 23:15:32 +0100

    Fix to failing flw.base tests.

--- test/flutterwave.base.tests.js
+++ test/flutterwave.base.tests.js
@@ -52,7 +52,7 @@ describe('#FlutterwaveBaseTest', function () {
 	it('Should Decrypt "PjBSczoWdA4=" using the key "Tk_ksodk" and return "SMS"', function ()
 	{
 		var flwbase = new flwBase(83948, 98898);
-		expect(flwbase.decryptText("PjBSczoWdA4=", "Tk_ksodk")).to.equal("SMS");
+		expect(flwbase.decryptText("Tk_ksodk")("PjBSczoWdA4=")).to.equal("SMS");
 	});
 
 	it('Should return with a 404', function (done) {
